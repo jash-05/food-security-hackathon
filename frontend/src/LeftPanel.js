99c5d5b74a42f70a97f165593c70b64bb1a0993f
jash-05 <jash.sheth@gmail.com>
2022-11-19 20:46:48 -0800

    added leftbar indicators

--- frontend/src/LeftPanel.js
+++ frontend/src/LeftPanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
@@ -7,83 +7,68 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
+import { indicatorMappings } from "./utils.py/constants";
 
 const LeftPanel = () => {
-  return (
-    <div className="panel-wrapper">
-      <div className="panel-dropdowns">
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>MacroEconomics</Accordion.Header>
-            <Accordion.Body>
-              <Box
-                sx={{
-                  width: "100%",
-                  maxWidth: 380,
-                  bgcolor: "background.paper",
-                }}
-              >
-                <nav aria-label="macroeconmoics-items">
-                  <List>
-                    <ListItem disablePadding>
-                      <ListItemButton>
-                        <ListItemText primary="Graph 1" />
-                      </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemButton component="a" href="#simple-list">
-                        <ListItemText primary="Graph 2" />
-                      </ListItemButton>
-                    </ListItem>
-                  </List>
-                </nav>
-              </Box>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Agricultural</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="2">
-            <Accordion.Header>Debt</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="3">
-            <Accordion.Header>Crops</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-      </div>
-      <div className="panel-buttons">
-        <Button variant="outlined">Import/Export</Button>
-        <Button variant="outlined">Yield</Button>
-      </div>
-    </div>
-  );
+	const [selectedStage, setSelectedStage] = useState("");
+	return (
+		<div className="panel-wrapper">
+			<div className="panel-dropdowns">
+				<Accordion defaultActiveKey="0">
+					{indicatorMappings.map((category) => {
+						return (
+							<Accordion.Item eventKey={category.eventKey}>
+								<Accordion.Header>
+									{category.text}
+								</Accordion.Header>
+								<Accordion.Body>
+									<Box
+										sx={{
+											width: "100%",
+											maxWidth: 380,
+											bgcolor: "background.paper",
+										}}
+									>
+										<nav
+											aria-label={category.key + "-items"}
+										>
+											<List>
+												{category.indicators.map(
+													(indicator) => {
+														return (
+															<ListItem
+																disablePadding
+															>
+																<ListItemButton>
+																	<ListItemText
+																		primary={
+																			indicator.text
+																		}
+																	/>
+																</ListItemButton>
+															</ListItem>
+														);
+													}
+												)}
+											</List>
+										</nav>
+									</Box>
+								</Accordion.Body>
+							</Accordion.Item>
+						);
+					})}
+					<Accordion.Item eventKey="3">
+						<Accordion.Header>Crops</Accordion.Header>
+						<Accordion.Body></Accordion.Body>
+					</Accordion.Item>
+				</Accordion>
+			</div>
+			<div className="panel-buttons">
+				<Button variant="outlined">Import/Export</Button>
+				<Button variant="outlined">Yield</Button>
+			</div>
+		</div>
+	);
 };
 
 export default LeftPanel;
