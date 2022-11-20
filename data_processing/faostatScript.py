2c80e9537304f695919243bb45106943fc019f48
jash-05 <jash.sheth@gmail.com>
2022-11-19 18:04:33 -0800

    added scripts for processing exports data

--- data_processing/faostatScript.py
+++ data_processing/faostatScript.py
@@ -9,22 +9,6 @@ db = client["hackathon"]
 
 path = "./../data/imports/imports.csv"
 
-countries = [
-    "Egypt",
-    "Saudi Arabia"
-]
-
-years = [
-    2018,
-    2019,
-    2020
-]
-
-products = [
-    "Wheat",
-    "Rice, paddy (rice milled equivalent)",
-    "Bananas"
-]
 collection = db["Imports"]
 data = list(csv.reader(open(path)))
 all_documents = []
@@ -37,4 +21,24 @@ for row in data[1:]:
         'value': int(row[13]) if row[13] else 0
     }
     all_documents.append(document)
-collection.insert_many(all_documents)
+# collection.insert_many(all_documents)
+
+
+def uploadDataForExport(export):
+    path = f"./../data/exports/{export}.csv"
+    collection = db[export]
+    data = list(csv.reader(open(path)))
+    all_documents = []
+    for row in data[1:]:
+        document = {
+            'year': int(row[0]),
+            'area_harvested': int(row[1]),
+            'production': int(row[2]),
+            'yield': int(row[3])
+        }
+        all_documents.append(document)
+    collection.insert_many(all_documents)
+
+
+uploadDataForExport("Philippines_Exports")
+uploadDataForExport("Iran_Exports")
